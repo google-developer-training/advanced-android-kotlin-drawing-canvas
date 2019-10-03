4f7e847837d0f3cb0fff91f47460bb09ec64a5c0
Murat Yener <yenerm@google.com>
2019-10-03 14:37:05 -0700

    Squashed commit of the following:
    commit 7e5e245a492c87f77338fa6ab1d8267c4b0970cc
    Author: Aleks Haecky <vhaecky@vhaecky-macbookpro.roam.corp.google.com>
    Date:   Wed Sep 25 14:30:48 2019 -0700
    
        Initial commit for murat
    
        Change-Id: Ice255df9b75e88f3740f823c70c075b1870703fc
    
    commit 16eacb04e06c4a7bb2d690299c415e9e4c8eb2a0
    Author: Aleks Haecky <vhaecky@google.com>
    Date:   Wed Sep 25 21:04:31 2019 +0000
    
        Initial empty repository

---
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.minipaint
+
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.appcompat.app.AppCompatActivity
+import com.example.minipaint.R
+
+/**
+ * Mini Paint shows how to create a custom view and draw on its canvas.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val myCanvasView = MyCanvasView(this)
+        // No XML file; just one custom view created programmatically.
+        // Request the full available screen for layout.
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+    }
+}
